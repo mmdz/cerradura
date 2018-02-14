61446a3875e5e9a1ac4a2a96b75c924b1e931ebf
Mell <mellmendez4089@gmail.com>
2018-02-14 18:04:21 -0300

    Primer commit, ej 2 cerradura

---
+++ src/ar/edu/unlam/pb2/Cerradura2.java
@@ -0,0 +1,108 @@
+package ar.edu.unlam.pb2;
+
+public class Cerradura2 {
+
+	//atributos 
+	private Boolean estado;
+	private Integer intentosMaximos;
+	private Integer claveAbreCierra;
+	private Integer cantidadIntentosFallidos;
+	private Boolean estaBloqueada;
+	
+	//constructor
+	public Cerradura2(){ //sin parametros
+	
+		this.claveAbreCierra = 1234;  //inicializo los atributos
+		this.estaBloqueada = false; //no bloqueada
+		this.estado = true; //abierta
+		this.cantidadIntentosFallidos = 0;
+		this.intentosMaximos = 0;
+	}
+	
+	public Cerradura2(Integer clave, Integer intentosMaxi){ //toma un nuevo valor recibido como parametro en el constructor
+
+		this.claveAbreCierra = clave; //tomara los  valores de los parametros nuevos
+		this.intentosMaximos = intentosMaxi;
+
+		this.estaBloqueada = false;
+		this.estado = true;
+		this.cantidadIntentosFallidos = 0 ;
+	}
+	
+//metodos
+	public Boolean abrir(Integer clave){
+		if(claveAbreCierra.equals(clave) && estaBloqueada.equals(false)){
+			estado =  true;
+			cantidadIntentosFallidos = 0; //resetea la cant cuando es correcta la clave
+		}
+		else{
+			cantidadIntentosFallidos+=1; 
+			if(cantidadIntentosFallidos.equals(intentosMaximos)){  //los intentos max se lo digo en el test
+				estaBloqueada = true; //se bloquea
+			
+			estado = false;
+		
+			}
+		}
+		return this.estado;
+
+	}
+	
+	public Boolean cerrar(Integer clave){
+		if(claveAbreCierra.equals(clave)){
+			estado = false; 
+		}
+		else{
+			estado = true; 
+		}
+		
+		return estado;
+	}
+	
+	public Boolean mostrarEstado(){
+		return this.estado;
+	}
+
+	
+	
+//creo equals y  hashcode para poder igualar objetos en el test	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((cantidadIntentosFallidos == null) ? 0 : cantidadIntentosFallidos.hashCode());
+		result = prime * result + ((claveAbreCierra == null) ? 0 : claveAbreCierra.hashCode());
+		result = prime * result + ((intentosMaximos == null) ? 0 : intentosMaximos.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Cerradura2 other = (Cerradura2) obj;
+		if (cantidadIntentosFallidos == null) {
+			if (other.cantidadIntentosFallidos != null)
+				return false;
+		} else if (!cantidadIntentosFallidos.equals(other.cantidadIntentosFallidos))
+			return false;
+		if (claveAbreCierra == null) {
+			if (other.claveAbreCierra != null)
+				return false;
+		} else if (!claveAbreCierra.equals(other.claveAbreCierra))
+			return false;
+		if (intentosMaximos == null) {
+			if (other.intentosMaximos != null)
+				return false;
+		} else if (!intentosMaximos.equals(other.intentosMaximos))
+			return false;
+		return true;
+	}
+	
+	
+	
+}
---
+++ test/ar/edu/unlam/pb2/TestCerradura2.java
@@ -0,0 +1,70 @@
+package ar.edu.unlam.pb2;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+
+public class TestCerradura2 {
+
+	
+	@Test 
+	public void testQueCreaCerradura(){
+		Cerradura2 miCerradura = new Cerradura2();
+		Assert.assertTrue(miCerradura.mostrarEstado()); //crea abierta
+	}
+	
+	
+	@Test
+	public void testQueAbre() {
+		Cerradura2 miCerradura = new Cerradura2(1234,4);	
+		miCerradura.abrir(123);
+		
+		Assert.assertTrue(miCerradura.mostrarEstado()); //true abierto
+	}
+
+	@Test 
+	public void testQueBloquea(){
+		Cerradura2 miCerradura = new Cerradura2(1234,4);
+		miCerradura.abrir(123);
+		miCerradura.abrir(2);
+		miCerradura.abrir(124);
+		miCerradura.abrir(2);
+		
+		Assert.assertFalse(miCerradura.mostrarEstado()); //si el estado es falso 
+	}
+	
+	@Test
+	public void testQueCierra() {		
+		Cerradura2 miCerradura = new Cerradura2();	
+		miCerradura.cerrar(1234);
+		
+		Assert.assertFalse(miCerradura.mostrarEstado());//tiene q dar falso para q este cerrado
+	}
+	
+	@Test
+	public void testQueNoCierra() {
+		Cerradura2 miCerradura = new Cerradura2(1234,4);	
+		miCerradura.cerrar(111);
+		
+		Assert.assertTrue(miCerradura.mostrarEstado());//tiene q dar true para q este abierto
+	}
+
+		
+	@Test 
+	public void testQueIgualaCerradura(){
+		Cerradura2 miCerradura_A = new Cerradura2(1234,4);
+		Cerradura2 miCerradura_B = new Cerradura2(1234,4);
+
+		Assert.assertEquals(miCerradura_A, miCerradura_B);//tiene q ser igual
+	}		
+		
+	@Test 
+	public void testQueNoIgualaCerradura(){
+		Cerradura2 miCerradura_A = new Cerradura2(1111,4);
+		Cerradura2 miCerradura_B = new Cerradura2(1234,4);
+
+		Assert.assertNotEquals(miCerradura_A, miCerradura_B);//tiene q ser diferente
+	}		
+	
+	
+}
